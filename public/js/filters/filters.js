0456b835106b603f748650a34724ae7b03954b93
Tyler Renelle <tylerrenelle@gmail.com>
2013-11-06 11:21:42 -0800

    [#1499] start moving todo completed / non-completed from taskClasses => filter

--- public/js/filters/filters.js
+++ public/js/filters/filters.js
@@ -3,8 +3,14 @@ angular.module('habitrpg')
     return function (gp) {
       return Math.floor(gp);
     }
-  }).filter('silver', function () {
+  })
+  .filter('silver', function () {
     return function (gp) {
       return Math.floor((gp - Math.floor(gp))*100);
     }
-  });
+  })
+//  .filter('todosFilter', function(){
+//    return function(task, scope) {
+//      return (task.type == 'todo') ? ((scope.list && scope.list.showCompleted) ? !task.completed : task.completed) : true;
+//    }
+//  })
